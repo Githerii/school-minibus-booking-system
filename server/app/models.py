fd8d572529c0fd29568e36a1276b023c76f8742e
David <nnjui13@gmail.com>
2026-01-27 22:25:59 +0300

    Defined Bus class and its relationships

--- server/app/models.py
+++ server/app/models.py
@@ -34,3 +34,19 @@ class Driver(db.Model):
     email = db.Column(db.String, unique=True)
 
     buses = db.relationship("Bus", back_populates="driver")
+
+class Bus(db.Model):
+    __tablename__ = "buses"
+
+    bus_id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey("routes.route_id"), nullable=False)
+    driver_id = db.Column(db.Integer, db.ForeignKey("drivers.driver_id"), nullable=False)
+    plate_number = db.Column(db.String, unique=True, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    gps_coordinates= db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    route = db.relationship("Route", back_populates="buses")
+    driver = db.relationship("Driver", back_populates="buses")
+    bookings = db.relationship("Booking", back_populates="bus")
+    
