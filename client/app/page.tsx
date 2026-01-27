6ae722818d66784f3f8eeb8254026c43db06a077
SirAlex36 <alexngenoo@gmail.com>
2026-01-27 23:06:34 +0300

    created header

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold">
+        Welcome to the Home Page
+      </h1>
+      {/* Other page-specific content goes here */}
+    </>
+  )
+}
