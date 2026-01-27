f31e474639e68e808cbb10149cf80d262dcee5b5
Githerii <kuiranathan@gmail.com>
2026-01-27 21:01:45 +0300

    added password to the parent model as well as the tablename i had missed

--- server/app/models.py
+++ server/app/models.py
@@ -5,7 +5,12 @@ db = SQLAlchemy()
 
 
 class Parent(db.Model):
+    __tablename__ = 'parents'
+
     parent_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
