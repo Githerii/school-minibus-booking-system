a3f4e6deb1c5fb637150cbf62cd3087172bd0815
root <root@Zakaach.localdomain>
2026-01-28 21:20:27 +0300

    intializing Home

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,3 +4,6 @@ import { Features } from "@/components/features"
 import { HowItWorks } from "@/components/how-it-works"
 import { CTA } from "@/components/cta"
 import { Footer } from "@/components/footer"
+
+export default function Home() {
+  return (
