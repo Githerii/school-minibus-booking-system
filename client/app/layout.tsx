c53f9567789e5ceff36b03e0353c8fdb94a3bed6
SirAlex36 <alexngenoo@gmail.com>
2026-01-28 15:53:24 +0300

    cto page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Header } from "../components/header";
 import { Hero } from "@/components/hero";
 import { HowItWorks } from "@/components/how-it-works"
+import { CTA } from "@/components/cta";
 import { Features } from "@/components/features";
 
 export default function RootLayout({
@@ -16,6 +17,7 @@ export default function RootLayout({
         <Hero />
         <Features />
         <HowItWorks />
+        <CTA />
         <main>{children}</main>
       </body>
     </html>
