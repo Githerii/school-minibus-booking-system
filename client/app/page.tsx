9a452c2ad033065a7d5c06fdba7c1a6ff5a2a98a
root <root@Zakaach.localdomain>
2026-01-28 21:19:08 +0300

    imports

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,18 +4,3 @@ import { Features } from "@/components/features"
 import { HowItWorks } from "@/components/how-it-works"
 import { CTA } from "@/components/cta"
 import { Footer } from "@/components/footer"
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <Features />
-        <HowItWorks />
-        <CTA />
-      </main>
-      <Footer />
-    </div>
-  )
-}
