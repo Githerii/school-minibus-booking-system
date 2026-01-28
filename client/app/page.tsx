9bc6440b000c374dda945ce4866793b3d32acc6a
SirAlex36 <alexngenoo@gmail.com>
2026-01-28 15:01:34 +0300

    fixed tailwind

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl font-bold">
-        Welcome to the Home Page
+       
       </h1>
       {/* Other page-specific content goes here */}
     </>
