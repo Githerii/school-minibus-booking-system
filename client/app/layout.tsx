c68ae417bafd7a92c82b80bc5b5359ca3fb0a234
root <root@Zakaach.localdomain>
2026-01-28 21:30:30 +0300

    exporting an d finnally calling the comps

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -29,3 +29,18 @@ export const metadata: Metadata = {
     apple: '/apple-icon.png',
   },
 }
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`font-sans antialiased`}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
+}
