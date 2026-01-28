524475ce4b69476cfcef3c633048b8e673bfed25
root <root@Zakaach.localdomain>
2026-01-28 21:21:18 +0300

    calling the components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,3 +7,15 @@ import { Footer } from "@/components/footer"
 
 export default function Home() {
   return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
+  )
+}
