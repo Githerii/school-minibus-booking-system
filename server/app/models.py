b18f01195f6f7b98cadf0b061ed6387457d52921
Githerii <kuiranathan@gmail.com>
2026-02-02 01:44:34 +0300

    updated the models to add the role of the user

--- server/app/models.py
+++ server/app/models.py
@@ -11,7 +11,8 @@ class Parent(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     full_name = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    role = db.Column(db.String(20), nullable=False, default="user")
+    created_at = db.Column(db.DateTime)
 
     bookings = db.relationship("Booking", back_populates="parent")
 
