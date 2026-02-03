dfd2218b1643c578a8e958d2cf2a4ed8fe4041c2
Githerii <kuiranathan@gmail.com>
2026-02-03 09:54:22 +0300

    Improved to a better color

--- client/components/admin/DashboardLayout.tsx
+++ client/components/admin/DashboardLayout.tsx
@@ -10,7 +10,7 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-gradient-to-br from-gray-50 to-blue-50/30">
       {/* Sidebar */}
       <Sidebar />
       
