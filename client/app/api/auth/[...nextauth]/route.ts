8c9b2c49a02f262c9f5a79241b7056ba745ccfa3
zakaach6712 <zakaachzack@gmail.com>
2026-02-09 17:09:56 +0300

    feat: add NextAuth route

---
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
