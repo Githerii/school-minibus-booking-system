c51587e14199711ae70086167d5bb219cc8bfb1e
herman <herman.kariuki@student.moringaschool.com>
2026-02-10 17:11:37 +0300

    flex screen background

--- client/components/admin/DashboardLayout.tsx
+++ client/components/admin/DashboardLayout.tsx
@@ -10,7 +10,7 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="flex h-screen bg-gradient-to-br from-gray-50 to-blue-50/30">
+    <div className="flex h-screen bg-gradient-to-br from-gray-50 to-blue-50/30 dark:bg-gradient-to-br dark:from-gray-900 dark:to-gray-800 transition-colors duration-300">
       {/* Sidebar */}
       <Sidebar />
       
