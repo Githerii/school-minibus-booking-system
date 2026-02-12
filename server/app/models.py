fa5a6bd0384f5f0c2b7d344e5bef84446915f9de
SirAlex36 <alexngenoo@gmail.com>
2026-02-12 21:43:57 +0300

    new migration

--- server/app/models.py
+++ server/app/models.py
@@ -10,7 +10,7 @@ class Parent(db.Model):
     parent_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     full_name = db.Column(db.String(100), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False, default="parent")
     created_at = db.Column(db.DateTime)
 
